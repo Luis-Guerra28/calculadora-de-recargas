6f4d481a3f39b22838f6b1d980b32cce8d377948
Luis <luis910gg@gmail.com>
2024-01-16 16:41:31 -0400

    Correcion de errores para la muestra en pantalla

--- app/main.py
+++ app/main.py
@@ -9,55 +9,39 @@ def main(page: ft.Page):
         
         value = int(textField_input.value)
         company = company_selection.value
-        listValues = DIGITEL_LIST if company else MOVISTAR_LIST
+        listValues = DIGITEL_LIST if company == 1 else MOVISTAR_LIST
         isMax = checkBox_valueMax.value
 
         if isMax: 
             porcentage = value * 0.2
             result = calcularSuma(value-porcentage, listValues)
-            
-            valueMin = result[0]
-            totalMin = porcentage + valueMin
-            combinationMin = result[2]
-            
-            valueMax = result[1]
-            totalMax = porcentage + valueMax
-            combinationMax = result[3]
-
-            max_recharge.controls[1].value = valueMax
-            max_porcentage.controls[1].value = porcentage
-            max_total_recharge.controls[1].value = totalMax
-            text_combinationMax.value = combinationMax
-            
-            min_recharge.controls[1].value = valueMin
-            min_porcentage.controls[1].value = porcentage
-            min_total_recharge.controls[1].value = totalMin
-            text_combinationMin.value = combinationMin
-            
-
 
         else:
             result = calcularSuma(value, listValues)
-            
-            valueMin = result[0]
-            porcentageMin = valueMin * 0.2
-            totalMin = porcentageMin + valueMin
-            combinationMin = result[2]
 
-            valueMax = result[1]
-            porcentageMax = valueMax * 0.2
-            totalMax = porcentageMax + valueMax
-            combinationMax = result[3]
+        # Recarga Maxima
+        valueMax = result[1]
+        porcentageMax = valueMax * 0.2
+        totalMax = porcentageMax + valueMax
+        combinationMax = result[3]
 
-            max_recharge.controls[1].value = valueMax
-            max_porcentage.controls[1].value = porcentageMax
-            max_total_recharge.controls[1].value = totalMax
-            text_combinationMax.value = combinationMax
-            
-            min_recharge.controls[1].value = valueMin
-            min_porcentage.controls[1].value = porcentageMin
-            min_total_recharge.controls[1].value = totalMin
-            text_combinationMin.value = combinationMin
+        # En pantalla (max)
+        max_recharge.controls[1].value = valueMax
+        max_porcentage.controls[1].value = porcentageMax
+        max_total_recharge.controls[1].value = totalMax
+        text_combinationMax.value = combinationMax
+
+        # Recarga Minima
+        valueMin = result[0]
+        porcentageMin = valueMin * 0.2
+        totalMin = porcentageMin + valueMin
+        combinationMin = result[2]
+
+        # En pantalla (max)
+        min_recharge.controls[1].value = valueMin
+        min_porcentage.controls[1].value = porcentageMin
+        min_total_recharge.controls[1].value = totalMin
+        text_combinationMin.value = combinationMin
 
         page.update()
 
@@ -84,7 +68,7 @@ def main(page: ft.Page):
             ft.Radio(value=0, label="Movistar"),
         ])
     )
-    
+
 
     #MAX CONTAINER
     max_recharge = ft.Row(
@@ -127,12 +111,12 @@ def main(page: ft.Page):
 
     min_recharge = ft.Row(
         controls = [
-            ft.Text("Valor de la recarga (por arriba)"),
+            ft.Text("Valor de la recarga (por abajo)"),
             ft.Text("00.00"),
         ],
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
     )
-    
+
     min_porcentage = ft.Row(
         controls = [
             ft.Text("20%"),
@@ -140,7 +124,7 @@ def main(page: ft.Page):
         ],
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
     )
-    
+
     min_total_recharge = ft.Row(
         controls = [
             ft.Text("Total a pagar"),
